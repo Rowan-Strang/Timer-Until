046196208779c9df89345de77d030f1a2eabbe79
Rowan Strang <rowanstrang@gmail.com>
2025-10-09 16:21:27 +1300

    Initial Commit

---
+++ Timer Until/Timer_UntilApp.swift
@@ -0,0 +1,17 @@
+//
+//  Timer_UntilApp.swift
+//  Timer Until
+//
+//  Created by MacRow on 09/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Timer_UntilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
